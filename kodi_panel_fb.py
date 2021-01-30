5cdf5b664724d3b8f51fea32888e5972857a7e5a
nico1080 <bonnaud.nico@gmail.com>
2021-01-31 00:17:27 +0100

    add acodeclogo for videos

--- kodi_panel_fb.py
+++ kodi_panel_fb.py
@@ -30,6 +30,25 @@ from time import sleep
 import config
 import kodi_panel_display
 
+import sys
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+from datetime import datetime, timedelta
+from aenum import Enum, extend_enum
+from functools import lru_cache
+import copy
+import time
+import logging
+import requests
+import json
+import io
+import re
+import os
+import threading
+import warnings
+
 # -------------------------------custom_function-----------------------------
 # channel number lookup
 ch_N = {
@@ -117,6 +136,39 @@ def calc_progress_custom(time_str, duration_str, layout_name):
 
 kodi_panel_display.calc_progress  = calc_progress_custom
 
+#----------------- codec images-------------------------
+codec_logo = {
+    "ac3": "images/codeclogo/audio/ac3.png",
+    "eac3": "images/codeclogo/audio/eac3.png",
+    "dtshd_ma": "images/codeclogo/audio/dtshd_ma.png",
+    "dtshd_hra": "images/codeclogo/audio/dtshd_hra.png",
+    "dts": "images/codeclogo/audio/dts.png",
+    "truehd": "images/codeclogo/audio/truehd.png",
+    "wmapro": "images/codeclogo/audio/wmapro.png",
+    "mp3float": "images/codeclogo/audio/mp3float.png",
+    "flac": "images/codeclogo/audio/flac.png",
+    "alac": "images/codeclogo/audio/alac.png",
+    "vorbis": "images/codeclogo/audio/vorbis.png",
+    "aac": "images/codeclogo/audio/aac.png",
+    "pcm_s16be": "images/codeclogo/audio/pcm.png",
+    "pcm_u8": "images/codeclogo/audio/pcm.png",
+    "h264": "images/codeclogo/audio/h264.png",
+    "wvc1": "images/codeclogo/video/wvc1.png",
+    "vc1": "images/codeclogo/video/wvc1.png",
+}
+
+def element_acodeclogo(image, draw, info, field, screen_mode, layout_name):
+    if 'VideoPlayer.AudioCodec' in info:
+        if info['VideoPlayer.AudioCodec'] in codec_logo:
+            logoimg = Image.open(codec_logo[info['VideoPlayer.AudioCodec']])
+            image.paste(logoimg, (field["posx"], field["posy"]))
+            return ""
+        else:
+            return ""
+
+    return ""
+
+kodi_panel_display.ELEMENT_CB["acodeclogo"] = element_acodeclogo
 
 #-----------------------------------------------------------------------------
 
